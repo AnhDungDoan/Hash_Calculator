686ac5a076796d8945abc2e6cfb559219e9fcb1b
GnudnaoD <khongphaidoandung@gmail.com>
2021-06-18 10:25:55 +0700

    update copyright

--- HashCalculator/HashCalculator/mainGUI.Designer.cs
+++ HashCalculator/HashCalculator/mainGUI.Designer.cs
@@ -49,6 +49,7 @@ namespace HashCalculator
             this.ExitButton = new System.Windows.Forms.Button();
             this.importFileButton = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -254,11 +255,22 @@ namespace HashCalculator
             this.label3.Size = new System.Drawing.Size(0, 20);
             this.label3.TabIndex = 14;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 585);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(209, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "© Doan Anh Dung ☞  Do not REUP";
+            // 
             // mainGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1042, 610);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.importFileButton);
             this.Controls.Add(this.ExitButton);
@@ -308,6 +320,7 @@ namespace HashCalculator
         private System.Windows.Forms.Button ExitButton;
         private System.Windows.Forms.Button importFileButton;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
